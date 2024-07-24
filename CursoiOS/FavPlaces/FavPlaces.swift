b0fc40b7601aa06a2457fcfb8a30bfd08475faca
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-07-24 13:21:43 -0600

    save places in a list and clicking takes us to them

--- CursoiOS/FavPlaces/FavPlaces.swift
+++ CursoiOS/FavPlaces/FavPlaces.swift
@@ -14,75 +14,119 @@ struct FavPlaces: View {
             center: CLLocationCoordinate2D(
                 latitude: 19.3990063,
                 longitude: -99.1171268),
-            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5	)
+            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
         )
     )
     
-    @State var places:[Place] = []
+    @State var places: [Place] = []
+    @State var showPopUp: CLLocationCoordinate2D? = nil
+    @State var showSheet: Bool = false
+    @State var name: String = ""
+    @State var fav: Bool = false
     
-//    @State var showPopUp:Bool = false
-    @State var showPopUp:CLLocationCoordinate2D? = nil
-    
-    @State var name:String = ""
-    @State var fav:Bool = false
+    let heigt = stride(from: 0.3, through: 0.3, by: 0.1).map { PresentationDetent.fraction($0) }
     
     var body: some View {
-        ZStack{
-            MapReader{ proxy in
-                Map(position: $position){
-                    ForEach(places){place in
-                        Annotation(place.name, coordinate: place.coordinates){
-                            let color = if place.fav { Color.yellow }else{ Color.black }
+        ZStack {
+            MapReader { proxy in
+                Map(position: $position) {
+                    ForEach(places) { place in
+                        Annotation(place.name, coordinate: place.coordinates) {
+                            let color = place.fav ? Color.yellow : Color.black
                             Circle()
                                 .stroke(color, lineWidth: 3)
                                 .fill(.white)
                                 .frame(width: 35, height: 35)
                         }
-                        
                     }
                 }
-                    .onTapGesture { coord in
-                        if let coordinates = proxy.convert(coord, from: .local){
-                            showPopUp = coordinates
-                        }
+                .onTapGesture { coord in
+                    if let coordinates = proxy.convert(coord, from: .local) {
+                        showPopUp = coordinates
                     }
+                }
+                .overlay {
+                    VStack {
+                        Button("Show list") {
+                            showSheet = true
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background(.white)
+                        .cornerRadius(16)
+                        .padding(16)
+                        Spacer()
+                    }
+                }
             }
             
-            if showPopUp != nil{
-                
-                let view = VStack{
-                    
+            if showPopUp != nil {
+                let view = VStack {
                     Text("Añadir localizacion").font(.title2).bold()
                     Spacer()
                     TextField("Nombre", text: $name).padding(.bottom, 8)
                     Toggle("Es un lugar favorito?", isOn: $fav)
                     Spacer()
-                    Button("Guardar"){
+                    Button("Guardar") {
                         savePlace(name: name, fav: fav, coordinates: showPopUp!)
                         clearForm()
                     }
                 }
-                withAnimation{
+                withAnimation {
                     CustomDialog(closeDialog: {
                         showPopUp = nil
                     }, onDismissOutside: true, content: view)
                 }
             }
-            
+        }
+        .sheet(isPresented: $showSheet) {
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(places) { place in
+                        let color = place.fav ? Color.yellow.opacity(0.5) : Color.black.opacity(0.5)
+                        VStack {
+                            Text(place.name).font(.title3).bold()
+                        }
+                        .frame(width: 150, height: 100)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20).stroke(color, lineWidth: 1)
+                        }
+                        .shadow(radius: 5)
+                        .padding(.horizontal, 8)
+                        .onTapGesture {
+                            animateCamera(cordinates: place.coordinates)
+                            showSheet = false
+                        }
+                    }
+                }
+            }
+            .presentationDetents(Set(heigt))
         }
     }
     
-    func savePlace(name:String, fav:Bool, coordinates:CLLocationCoordinate2D){
-        let palce = Place(name: name, coordinates: coordinates, fav: fav)
-        places.append(palce)
+    func animateCamera(cordinates: CLLocationCoordinate2D) {
+        withAnimation {
+            position = MapCameraPosition.region(
+                MKCoordinateRegion(
+                    center: cordinates,
+                    span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+                )
+            )
+        }
     }
     
-    func clearForm(){
+    func savePlace(name: String, fav: Bool, coordinates: CLLocationCoordinate2D) {
+        let place = Place(name: name, coordinates: coordinates, fav: fav)
+        places.append(place)
+    }
+    
+    func clearForm() {
         name = ""
         fav = false
         showPopUp = nil
     }
 }
+
 
 #Preview {
     FavPlaces()
